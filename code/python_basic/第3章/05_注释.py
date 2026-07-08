7b8c43f2c609e965576623debddec5601b966c00
zhuohualuo666666-ops <zhuohualuo666666@gmail.com>
2026-07-08 08:10:26 +0800

    Create 05_注释.py

---
+++ code/python_basic/第3章/05_注释.py
@@ -0,0 +1,30 @@
+#coding=utf-8
+
+"""在该文件中主要学习了注释相关内容"""
+
+
+# name是张三的名字
+name = '张三'
+# age是张三的年龄
+age = 18
+# weight是张三的体重（单位：千克）
+weight = 60.2
+
+print(name, age, weight)  # 下面是一句打印
+
+'''
+下面是一些常量
+ADULT_AGE是法定成人年龄
+MONTHS_IN_YEAR 是一年中的几个月
+MAX_USERS是最大在线人数
+'''
+"""
+下面是一些常量
+ADULT_AGE是法定成人年龄
+MONTHS_IN_YEAR 是一年中的几个月
+MAX_USERS是最大在线人数
+"""
+ADULT_AGE = 18
+MONTHS_IN_YEAR = 12
+MAX_USERS = 1200
+print(ADULT_AGE, MONTHS_IN_YEAR, MAX_USERS)
