580c0a96e447b1a2bc90e9383f523880a8778fe4
zhuohualuo666666-ops <zhuohualuo666666@gmail.com>
2026-07-18 11:47:18 +0800

    Create 09_浮点型.py

---
+++ code/python_basic/第3章/09_浮点型.py
@@ -0,0 +1,19 @@
+weight = 65.2
+balance = 1425.58
+out_temp = -25.2
+price = 120.0
+print(type(price))
+
+# 浮点型科学计数法表示
+speed_of_sound = 3.4e+2  # 3.4乘10的2次方
+print(speed_of_sound)
+world_population = 7.8e9  # 7.8乘10的9次方
+print(world_population)
+distance_sun_earth = 1.496E8  # 1.496乘10的8次方
+print(distance_sun_earth)
+speed_of_light = 2.998E+8  # 2.998乘10的8次方
+print(speed_of_light)
+
+one_ml = 1e-3  # 1的-3次方
+one_mg = 1E-3  # 1的-3次方
+print(one_ml,one_mg)
