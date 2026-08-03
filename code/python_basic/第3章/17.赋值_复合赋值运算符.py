b23a9fbf6a1ba6c248292758089c4b45b706fd84
zhuohualuo666666-ops <zhuohualuo666666@gmail.com>
2026-08-03 11:23:35 +0800

    Create 17.赋值_复合赋值运算符.py

---
+++ code/python_basic/第3章/17.赋值_复合赋值运算符.py
@@ -0,0 +1,45 @@
+age = 18
+# # 加法复合运算符
+age += 1  # 等价于：age = age + 1
+print(age)
+
+price = 52
+freight = 6
+# # 加法复合运算符
+price += freight  # 等价于：price = price + freight
+print(price)
+
+# 减法复合运算符
+age = 18
+age -= 1  # 等价于：age = age - 1
+print(age)
+
+# 乘法复合运算符
+price = 100
+discount = 0.8
+price *= discount  # 等价于：price = price * discount
+print(price)
+
+# 除法复合运算符
+pay = 100
+num = 5
+pay /= 5  # 等价于：pay = pay / num
+print(pay)
+
+# 取整赋值运算符
+apple = 31
+num = 14
+apple //= num  # 等价于：apple = apple // num
+print(apple)
+
+# 取模赋值运算符
+seconds = 386
+minutes = 60
+seconds %= minutes  # 等价于：seconds = seconds % minutes
+print(seconds)
+
+# 指数赋值运算符
+a = 2
+b = 3
+a **= b  # 等价于：a = a ** b
+print(a)
