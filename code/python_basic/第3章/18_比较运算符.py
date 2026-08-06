81ddde8c2fb02a08e3484bd6a1eec8fb8c03b145
zhuohualuo666666-ops <zhuohualuo666666@gmail.com>
2026-08-06 13:57:00 +0800

    Create 18_比较运算符.py

---
+++ code/python_basic/第3章/18_比较运算符.py
@@ -0,0 +1,66 @@
+# 使用==判断左右两侧是否相等
+# a = 5
+# b = 7
+# c = '5'
+# result = a == c
+# print(result)
+
+# 使用!=判断左右两侧是否不等
+# a = 5
+# b = 7
+# c = '5'
+# result = a != c
+# print(result)
+
+# 使用>判断左侧是否大于右侧
+# a = 9
+# b = 7
+# c = '5'
+# result = a > b
+# print(result)
+
+# 使用<判断左侧是否小于右侧
+# a = 3
+# b = 7
+# c = '5'
+# result = a < b
+# print(result)
+
+# 使用>=判断左侧是否大于等于右侧
+# a = 6
+# b = 7
+# c = '5'
+# result = a >= b
+# print(result)
+
+# 使用<=判断左侧是否小于等于右侧
+# a = 9
+# b = 7
+# c = '5'
+# result = a <= b
+# print(result)
+
+# 以上这些比较运算符，同样适用于字符串
+# msg1 = 'abc'
+# msg2 = 'abc666'
+# print(msg1 == msg2)
+
+# msg1 = 'abc'
+# msg2 = 'abc'
+# print(msg1 != msg2)
+
+# 使用ord()查看指定字符的Unicode编码
+# print(ord('a'))
+# print(ord('我'))
+
+# 使用chr()将Unicode编码转为字符
+print(chr(97))
+print(chr(25105))
+
+msg1 = 'abc'
+msg2 = 'xyz'
+msg3 = '我爱你'
+msg4 = '中国'
+msg5 = 'abc'
+msg6 = 'abcdef'
+print(msg1 <= msg5)
