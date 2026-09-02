e1965fc0541f4b2df296803363a866cb36d8b5ad
zhuohualuo666666-ops <zhuohualuo666666@gmail.com>
2026-09-02 08:45:27 +0800

    Create 08_for循环案例.py

---
+++ code/python_basic/第4章/08_for循环案例.py
@@ -0,0 +1,13 @@
+# 加密代码
+text = input('📝请输入要加密的文字：')
+secret = ''
+for t in text:
+    secret += chr(ord(t) + 1)
+print(f'㊙️经过加密后的内容为：{secret}')
+
+# 解密代码
+# secret = input('📝请输入要解密的文字：')
+# text = ''
+# for s in secret:
+#     text += chr(ord(s) - 1)
+# print(f'📃经过解密后的内容为：{text}')
